4900dd7f36f22d9b3698af6b19ca7b0fb8dcfcc1
KamogeloKayG <kamzamabatha16@gmail.com>
2025-09-01 14:14:22 +0200

    initial commit

---
+++ MoneyHub/src/main/java/com/moneyhub/MoneyHub/MoneyHubApplication.java
@@ -0,0 +1,13 @@
+package com.moneyhub.MoneyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyHubApplication.class, args);
+	}
+
+}
---
+++ MoneyHub/src/test/java/com/moneyhub/MoneyHub/MoneyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyhub.MoneyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
